a34dc4cf4b82730544a85d959c4af420d66a1592
Pablo Rumualdo <pablorumualdo2305@gmail.com>
2023-10-27 09:38:22 -0300

    Integración del modal auth agus y logica simón P.1

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,25 @@
-import React, { ButtonHTMLAttributes } from 'react';
+'use client'
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps {
+    type?: 'button' | 'submit' | 'reset' | undefined
+    children?: React.ReactNode
+    onClick?: () => void
+    disabled?: boolean
+}
 
-const Button: React.FC<Props> = ({ children, ...props }) => {
+const Button: React.FC<ButtonProps> = ({ children, type, onClick, disabled }) => {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded" {...props}>
+    <button 
+        type={type} 
+        onClick={onClick} 
+        disabled={disabled}
+        className={`inline-flex w-full justify-center rounded-md
+        px-4 py-2 bg-gray-500 text-white
+        ${disabled && "opacity-50 cursor-default"}`}
+    >
       {children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,17 @@
 "use client";
 import Link from "next/link";
-import React from "react";
-import { UserAuth } from "../context/AuthContext";
+import React, { useState } from "react";
+import { UserAuth } from "@/context/AuthContext";
+import Modal from "./Modal";
+import AuthForm from "./AuthForm";
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen)
+  }
 
   const hanldeSignOut = async () => {
     try {
@@ -17,7 +24,7 @@ const Navbar = () => {
   console.log(user);
 
   return (
-    <div className="h-20 w-full border-b-2 flex items-center justify-between p-2 text-white">
+    <div className="h-20 w-full border-b-2 flex items-center justify-between p-2 text-black">
       <ul className="flex">
         <li className="p-2 cursor-pointer">
           <Link href="/">Home</Link>
@@ -32,14 +39,23 @@ const Navbar = () => {
         )}
       </ul>
       {!user ? (
-        <ul className="flex">
-          <li className="p-2 cursor-pointer">
-            <Link href="/login">Login</Link>
-          </li>
-          <li className="p-2 cursor-pointer">
-            <Link href="/register">Register</Link>
-          </li>
-        </ul>
+        // <ul className="flex">
+        //   <li className="p-2 cursor-pointer">
+        //     <Link href="/login">Login</Link>
+        //   </li>
+        //   <li className="p-2 cursor-pointer">
+        //     <Link href="/register">Register</Link>
+        //   </li>
+        // </ul>
+        <div>
+          <button type="button" onClick={toggleModal} className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
+            Start
+            <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+          </button>
+          <Modal toggleModal={toggleModal} isOpen={isOpen}>
+            <AuthForm />
+          </Modal>
+        </div>
       ) : (
         <div>
           <p>Welcome {user.email}</p>
