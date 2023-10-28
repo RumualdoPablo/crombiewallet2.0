dc185b282d2ee1483214571cebc43d9d1593c07a
Pablo Rumualdo <pablorumualdo2305@gmail.com>
2023-10-28 10:00:15 -0300

    Integracion del cambio de idioma en navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
 
   const hanldeSignOut = async () => {
     try {
-      await logOut();
+      logOut();
       setIsOpen(false)
     } catch (error) {
       console.log(error);
@@ -27,57 +27,51 @@ const Navbar = () => {
 
   return (
     <nav className="h-20 w-full border-b-2 flex items-center justify-between p-2 text-black">
-      <ul className="flex">
-        <li className="p-2 cursor-pointer">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="p-2 cursor-pointer">
-          <Link href="/about">About</Link>
-        </li>
-        {user && (
-          <li className="p-2 cursor-pointer">
-            <Link href={`/profile/${user.uid}`}>Profile</Link>
-          </li>
-        )}
-      </ul>
-
-      <div className='flex gap-x-1 '>
-      <IconWorld/>
-        <Link href="/" locale='en' className='border-r-[2px] border-black pr-1'>English</Link>
-        <Link href="/" locale='es' className=''>Español</Link>
+      <div className='flex gap-x-2'>
+        <IconWallet />
+        <h1>CrombieWallet</h1>
       </div>
-      
-      {!user ? (
-        <div>
-          <button type="button" onClick={toggleModal} className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
-            Start
-            <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-          </button>
-          <Modal toggleModal={toggleModal} isOpen={isOpen}>
-            <AuthForm />
-          </Modal>
-        </div>
-      ) : (
-        <div>
-          <div>
-            <p>Welcome {user.email}</p>
-          </div>
-          <button
-          type="button"
-          onClick={hanldeSignOut}
-          className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-          >
-            Sign Out
-          </button>
-        </div>
-      ) 
-      }
 
-    <Modal toggleModal={toggleModal} isOpen={isOpen}>
-      <AuthForm />
-    </Modal>
-  </nav>
-);
+      <div className='flex gap-x-4 items-center'>
+        <div className='flex gap-x-1'>
+        <IconWorld />
+        <Link href="/" locale='en' className='border-r-[2px] border-slate-300 pr-1'>English</Link>
+        <Link href="/" locale='es' className=''>Español</Link>
+        </div>
+
+
+        {!user ? (
+          <div>
+            <button type="button" onClick={toggleModal} className="text-white bg-emerald-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
+              Start
+              <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            </button>
+            <Modal toggleModal={toggleModal} isOpen={isOpen}>
+              <AuthForm />
+            </Modal>
+          </div>
+        ) : (
+          <div>
+            <div>
+              <p>Welcome {user.email}</p>
+            </div>
+            <button
+              type="button"
+              onClick={hanldeSignOut}
+              className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+            >
+              Sign Out
+            </button>
+          </div>
+        )
+        }
+      </div>
+
+      <Modal toggleModal={toggleModal} isOpen={isOpen}>
+        <AuthForm />
+      </Modal>
+    </nav>
+  );
 
 }
 
