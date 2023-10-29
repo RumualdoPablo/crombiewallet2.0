222c089c6aecb946886c63b802f42bc69d6e772b
Agustina <alcarazagustina01@gmail.com>
2023-10-29 11:36:20 -0300

    Agrego transicion suave entre modos oscuro/claro

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,22 +1,28 @@
-"use client"
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useTheme } from 'next-themes'
 
 const ThemeSwitch = () => {
-    const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme()
 
-    // useEffect only runs on the client, so now we can safely show the UI
-    useEffect(() => {
-        setMounted(true)
-    }, [])
-
-    if (!mounted) {
-        return null
+    const handleThemeChange = (newTheme: string) => {
+        if (newTheme === 'dark') {
+            document.documentElement.style.backgroundColor = '#000'
+            document.documentElement.style.color = '#fff'
+            setTimeout(() => {
+                setTheme(newTheme)
+            }, 300); // transicion suave de 0.3s
+        } else {
+            setTheme(newTheme)
+        }
     }
 
+    useEffect(() => {
+        document.documentElement.style.backgroundColor = theme === 'dark' ? '#000' : '#fff'
+        document.documentElement.style.color = theme === 'dark' ? '#fff' : '#000'
+    }, [theme])
+
     return (
-        <select value={theme} onChange={e => setTheme(e.target.value)}>
+        <select value={theme} onChange={(e) => handleThemeChange(e.target.value)}>
             <option value="system">System</option>
             <option value="dark">Dark</option>
             <option value="light">Light</option>
