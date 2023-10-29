d2bb9b7730ceaace97d5019fb63deecea72b0f93
Agustina <alcarazagustina01@gmail.com>
2023-10-29 10:38:17 -0300

    Alineo elementos del navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -28,22 +28,21 @@ const Navbar = () => {
 
   return (
     <nav className="h-20 w-full border-b-2 flex items-center justify-between p-2 text-black">
-      <div className='flex gap-x-2 items-center'>
-        <IconWallet size={33}/>
+      <div className='flex items-center'>
+        <IconWallet size={33} />
         <h1 className='text-xl font-bold dark-mode-font'>CrombieWallet</h1>
       </div>
 
       <div className='flex gap-x-4 items-center'>
-        <ThemeSwitch/>
-        <div className='flex gap-x-1 dark-mode-font'>
-        <IconWorld />
-        <Link href="/" locale='en' className='border-r-[2px] border-slate-300 pr-1'>English</Link>
-        <Link href="/" locale='es' className=''>Español</Link>
+        <ThemeSwitch />
+        <div className='flex gap-x-1 dark-mode-font items-center'>
+          <IconWorld />
+          <Link href="/" locale='en' className='border-r-[2px] border-slate-300 pr-1'>English</Link>
+          <Link href="/" locale='es' className=''>Español</Link>
         </div>
 
-
         {!user ? (
-          <div>
+          <div className="flex items-center">
             <button type="button" onClick={toggleModal} className="text-white bg-emerald-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
               Start
               <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
@@ -53,9 +52,9 @@ const Navbar = () => {
             </Modal>
           </div>
         ) : (
-          <div>
+          <div className="flex items-center">
             <div>
-              <p>Welcome {user.email}</p>
+              <p className="mr-2">Welcome {user.email}</p>
             </div>
             <button
               type="button"
