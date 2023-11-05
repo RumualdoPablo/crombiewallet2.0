901370033b25d8d27dd0a1eebc87d109d86ada0e
Agustina <alcarazagustina01@gmail.com>
2023-11-05 10:05:37 -0300

    agg funcion de mostrar movimientos por fecha

--- app/[locale]/dashboard/components/chart/LastRegisters.tsx
+++ app/[locale]/dashboard/components/chart/LastRegisters.tsx
@@ -1,39 +1,65 @@
-import { DataProps, expenses, incomes } from "@/interfaces/data"
+import React, { useState } from 'react';
+import { DataProps, expenses, incomes } from "@/interfaces/data";
 import { Text, Table, TableHead, TableRow, 
     TableHeaderCell, TableBody, 
-    TableCell, BadgeDelta } from "@tremor/react"
+    TableCell, BadgeDelta } from "@tremor/react";
 
 const LastRegisters: React.FC<DataProps> = ({ expenses, incomes, texts }) => {
-  
-  const expenseList:expenses[] = expenses || []
-  const incomeList: incomes[] = incomes || []
-  const transactions = [...expenseList, ...incomeList]
+  const expenseList: expenses[] = expenses || [];
+  const incomeList: incomes[] = incomes || [];
+  const transactions = [...expenseList, ...incomeList];
 
+  //agrego un estado para configurar el orden
+  const [sortConfig, setSortConfig] = useState({
+    key: 'date', //campos de orden por default 
+    direction: 'desc',
+  });
+
+  const sortedTransactions = [...transactions].sort((a, b) => {
+    if (a[sortConfig.key] < b[sortConfig.key]) {
+      return sortConfig.direction === 'asc' ? -1 : 1;
+    }
+    if (a[sortConfig.key] > b[sortConfig.key]) {
+      return sortConfig.direction === 'asc' ? 1 : -1;
+    }
+    return 0;
+  });
+
+  //funcion para cambiar el orden ascendente o descendente
+  const requestSort = (key: string) => {
+    let direction = 'asc';
+    if (sortConfig.key === key && sortConfig.direction === 'asc') {
+      direction = 'desc';
+    }
+    setSortConfig({ key, direction });
+  };
 
   return (
     <Table>
       <TableHead>
         <TableRow>
           <TableHeaderCell>{texts("tabs.transactions.description")}</TableHeaderCell>
-          <TableHeaderCell>{texts("tabs.transactions.date")}</TableHeaderCell>
+          <TableHeaderCell onClick={() => requestSort('date')}>
+            {texts("tabs.transactions.date")} {sortConfig.key === 'date' && (
+              sortConfig.direction === 'asc' ? '↑' : '↓'
+            )}
+          </TableHeaderCell>
           <TableHeaderCell>{texts("tabs.transactions.total")}</TableHeaderCell>
         </TableRow>
       </TableHead>
       <TableBody>
-        {transactions?.slice(0, 10).map((data) => (
+        {sortedTransactions.slice(0, 10).map((data) => (
           <TableRow key={data.id}>
             <TableCell>{data.description}</TableCell>
             <TableCell>
               <Text>{data.date.toDate().toLocaleString()}</Text>
             </TableCell>
-            <TableCell>
-              {data.amount}
-            </TableCell>
+            <TableCell>{data.amount}</TableCell>
           </TableRow>
         ))}
       </TableBody>
     </Table>
-  )
-}
+  );
+};
 
-export default LastRegisters
+export default LastRegisters;
