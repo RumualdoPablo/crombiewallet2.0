099a93e468030299b2d1ff879b2c8f72621db79a
Agustina <alcarazagustina01@gmail.com>
2023-11-05 11:54:56 -0300

    agg buttom view more en movimientos y barra de desplazamiento

--- app/[locale]/dashboard/components/chart/LastRegisters.tsx
+++ app/[locale]/dashboard/components/chart/LastRegisters.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { DataProps, expenses, incomes } from "@/interfaces/data";
-import { Text, Table, TableHead, TableRow, 
-    TableHeaderCell, TableBody, 
-    TableCell, BadgeDelta } from "@tremor/react";
+import {
+  Text, Table, TableHead, TableRow,
+  TableHeaderCell, TableBody,
+  TableCell, BadgeDelta, Title, Card, Button
+} from "@tremor/react";
 
 const LastRegisters: React.FC<DataProps> = ({ expenses, incomes, texts }) => {
   const expenseList: expenses[] = expenses || [];
@@ -11,10 +13,12 @@ const LastRegisters: React.FC<DataProps> = ({ expenses, incomes, texts }) => {
 
   //estado para configurar el orden
   const [sortConfig, setSortConfig] = useState({
-    key: 'date', //campos de orden por defecto 
+    key: 'date',
     direction: 'desc',
   });
-
+ //estado para controlar cuantos elementos se muestran
+  const [showAll, setShowAll] = useState(false);
+ //ordeno las transacciones por fecha
   const sortedTransactions = [...transactions].sort((a, b) => {
     if (a[sortConfig.key] < b[sortConfig.key]) {
       return sortConfig.direction === 'asc' ? -1 : 1;
@@ -24,8 +28,7 @@ const LastRegisters: React.FC<DataProps> = ({ expenses, incomes, texts }) => {
     }
     return 0;
   });
-
-  //funcion para cambiar el orden ascendente o descendente
+ //funcion para cambiar el orden ascendente o descendente
   const requestSort = (key: string) => {
     let direction = 'asc';
     if (sortConfig.key === key && sortConfig.direction === 'asc') {
@@ -35,32 +38,56 @@ const LastRegisters: React.FC<DataProps> = ({ expenses, incomes, texts }) => {
   };
 
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableHeaderCell>{texts("tabs.transactions.description")}</TableHeaderCell>
-          <TableHeaderCell onClick={() => requestSort('date')}>
-            {texts("tabs.transactions.date")} {sortConfig.key === 'date' && (
-              sortConfig.direction === 'asc' ? '↑' : '↓'
+    <Card>
+      <Title className='font-medium'>List of Movements</Title>
+      <div style={{ maxHeight: showAll ? '500px' : 'auto', overflowY: showAll ? 'auto' : 'visible' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell>{texts("tabs.transactions.description")}</TableHeaderCell>
+              <TableHeaderCell onClick={() => requestSort('date')}>
+                {texts("tabs.transactions.date")} {sortConfig.key === 'date' && (
+                  sortConfig.direction === 'asc' ? '↑' : '↓'
+                )}
+              </TableHeaderCell>
+              <TableHeaderCell>{texts("tabs.transactions.total")}</TableHeaderCell>
+              <TableHeaderCell>TYPE</TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {showAll
+              ? sortedTransactions.map((data) => (
+                <TableRow key={data.id}>
+                  <TableCell>{data.description}</TableCell>
+                  <TableCell>
+                    <Text>{data.date.toDate().toLocaleString()}</Text>
+                  </TableCell>
+                  <TableCell>{data.amount}</TableCell>
+                  <TableCell>{expenseList.includes(data) ? <BadgeDelta deltaType={"decrease"} /> : <BadgeDelta deltaType={"increase"} />}</TableCell>
+                </TableRow>
+              ))
+              : sortedTransactions.slice(0, 10).map((data) => (
+                <TableRow key={data.id}>
+                  <TableCell>{data.description}</TableCell>
+                  <TableCell>
+                    <Text>{data.date.toDate().toLocaleString()}</Text>
+                  </TableCell>
+                  <TableCell>{data.amount}</TableCell>
+                  <TableCell>{expenseList.includes(data) ? <BadgeDelta deltaType={"decrease"} /> : <BadgeDelta deltaType={"increase"} />}</TableCell>
+                </TableRow>
+              ))}
+
+            {!showAll && sortedTransactions.length > 10 && (
+              <TableRow>
+                <TableCell colSpan={4}>
+                  <Button onClick={() => setShowAll(true)}>View More</Button>
+                </TableCell>
+              </TableRow>
             )}
-          </TableHeaderCell>
-          <TableHeaderCell>{texts("tabs.transactions.total")}</TableHeaderCell>
-          <TableHeaderCell>TYPE</TableHeaderCell> 
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {sortedTransactions.slice(0, 10).map((data) => (
-          <TableRow key={data.id}>
-            <TableCell>{data.description}</TableCell>
-            <TableCell>
-              <Text>{data.date.toDate().toLocaleString()}</Text>
-            </TableCell>
-            <TableCell>{data.amount}</TableCell>
-            <TableCell>{expenseList.includes(data) ? <BadgeDelta deltaType={"decrease"} /> : <BadgeDelta deltaType={"increase"} />}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+          </TableBody>
+        </Table>
+      </div>
+    </Card>
   );
 };
 
